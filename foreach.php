241a838c7feda9f3da0ba0e34de39210588d3fee
harilpatel <84775177+harilpatel@users.noreply.github.com>
2021-05-26 07:05:50 -0700

    php2

---
+++ foreach.php
@@ -0,0 +1,16 @@
+<?php
+   
+      
+         $array = array( 1, 2, 3, 4, 5);
+         
+         foreach( $array as $value ) {
+            echo "Value is $value <br />";
+         }
+      ?>
+     
+<!----/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */--->
+
