8a163beba9c9ee670e6f4c511bb80cc584454088
harilpatel <84775177+harilpatel@users.noreply.github.com>
2021-05-28 06:02:50 -0700

    Add files via upload

---
+++ task2/array.php
@@ -0,0 +1,30 @@
+
+<html
+    <body>
+<?php
+//Variable
+//Method1
+$a = 10;
+$b = 20;
+//Method 1
+        echo "A value is ".$a;
+        echo "<br/>B value is".$b;
+        //Method 2
+        echo "<br/>A value is ".$a."<br/>BValue is ".$b;
+        //Method 3
+        echo "<br/>A value is $a<br/>B value is $b";
+        //Method 4
+        echo "<br/>A value is $a<br/>B value is $b";
+        //Method 5
+        echo "<br/>A value is $a<br/>B Value is $b sum is ";
+
+
+?>
+</body>
+</html>
+<!----/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */---->
+
--- task2/dowhile.php
+++ task2/dowhile.php
@@ -1,14 +1,19 @@
-<?php
+
    
-      
-         $array = array( 1, 2, 3, 4, 5);
+      <?php
+         $i = 0;
+         $num = 0;
          
-         foreach( $array as $value ) {
-            echo "Value is $value <br />";
+         do {
+            $i++;
          }
+         
+         while( $i < 10 );
+         echo ("Loop stopped at i = $i" );
       ?>
-     
-<!----/* 
+      
+ 
+<!-----/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
