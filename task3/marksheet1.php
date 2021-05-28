d927057f512e67a581a6c4e8760c623bd63a33bc
harilpatel <84775177+harilpatel@users.noreply.github.com>
2021-05-28 05:17:08 -0700

    Add files via upload

--- task3/marksheet1.php
+++ task3/marksheet1.php
@@ -1,1 +1,50 @@
+<html>
+    <body>
+        <form action="" method="post">
+            <center>
+				<h1>Marksheet</h1>
+                <table border=0 cellpadding=10px>
+                    <tr>
+                        <td>
+                            Student Name
+                        </td>
+                        <td>
+                            <input type=text name="t1" required >
+                        </td>
+                    </tr>
+					<br>
+                    <tr>
+                        <td>
+                            Marks for Physics
+                        </td>
+                        <td>
+                            <input type=text name="t2" required>
+                        </td>
+                    </tr>
+					<br>
+                    <tr>
+                        <td>
+                            Marks for Chemistry
+                        </td>
+                        <td>
+                            <input type=text name="t3" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Marks for Math
+                        </td>
+                        <td>
+                            <input type=text name="t4" required>
+                        </td>
+                    </tr>
+					<br>
+                </table>
+                <br>
+
+<!------/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */----->
 
