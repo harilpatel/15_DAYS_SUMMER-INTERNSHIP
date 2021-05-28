c5ca968feea4cbce38b2bea08788689e608f4c34
harilpatel <84775177+harilpatel@users.noreply.github.com>
2021-05-28 05:53:03 -0700

    Add files via upload

---
+++ task3/forminPOST.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Password: <input type="text" name="password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
