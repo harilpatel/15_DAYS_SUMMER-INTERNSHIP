6de4ea29041ed0821e6b9a9c157d27a8693a7538
harilpatel <84775177+harilpatel@users.noreply.github.com>
2021-06-04 10:40:45 -0700

    Add files via upload

---
+++ task7/connection.php
@@ -0,0 +1,31 @@
+<?php
+
+$connection = mysqli_connect("localhost","root","","db_internship");
+
+if($_POST){
+    $name = $_POST['txt1'];
+    $gender = $_POST['txt2'];
+    $mobile = $_POST['txt3'];
+    $q = mysqli_query($connection, "insert into tbl_user(user_name,user_gender,user_mobile) values('($name)','($gender)','($mobile)')") or die(mysqli_error($connection));
+    }if($q){
+        echo "<script>alert('Record Added');</script>";
+    }
+
+?>
+
+<html>
+    <body>
+        <form method="post">
+            name : <input type="text" name="txt1"/>
+            Gender : <select name="txt2">
+                <option>Male</option>
+                <option>Female</option>
+            </select>
+            Mobile No: <input type="number" name="txt3" />
+            <input type="submit" />
+            
+        </form>
+    </body>
+</html>
+
+echo "<a href = 'fetch.php'>Add Record</a>";
