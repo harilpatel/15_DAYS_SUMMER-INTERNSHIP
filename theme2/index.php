cd3f2e666fc7bbddab624deaf07b7cc35fe44d9d
harilpatel <84775177+harilpatel@users.noreply.github.com>
2021-06-05 23:57:53 -0700

    Add files via upload

---
+++ theme2/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+  Template 0258
+  Designed by CSS Templates For Free and licensed under the Creative Commons Attribution 3.0
+  Website: http://www.csstemplatesforfree.org/
+  Twitter: @cssforfree
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <title>Your Page Name</title>
+    <link rel="stylesheet" type="text/css" href="site-assets/screen.css" />
+  </head>
+  <body bgcolor="gray">
+    <div id="outer-container">
+      <div id="header">
+        <div id="main-logo">
+          <h1><a href="#">Web Developer</a></h1>
+        </div>
+        <div id="main-menu">
+          <ul>
+            <li class="first-item enabled-item">
+              <a href="#">Haril</a>
+            </li>
+            
+          </ul>
+          <br class="clear-all" />
+        </div>
+          
+      </div>
+      <div id="feature-box">
+                  <img src="images/pic1.jpg" width="980" height="310" alt=""/>
+      </div>
+      <div id="inner-container">
+        <div id="column1">
+          <div id="content1">
+            <h2>Welcome To Web developer</h2>
+            <p>
+              Welcome. This is another free CSS template created by <a href="http://www.csstemplatesforfree.org/">CSS Templates For Free</a>. It is released under the <a href="http://www.csstemplatesforfree.org/license">Creative Commons Attribution 3.0</a> license which permits you to use it personally and commercially provided you retain the footer link back to our website. All photos used in our free CSS templates are public domain photos provided by <a href="http://www.pdphoto.org">PDPhoto.org</a>.
+            </p>
+          </div>
+          <div id="content2">
+            <h3>Subheading</h3>
+              <h3>Subheading</h3>
+             <div class="news">
+            <ul class="links news">
+              <li class="first-item">
+                <span class="date">12/03</span> <a href="#"></a>
+              </li>
+              <li>
+                <span class="date">11/27</span> <a href="#"></a>
+              </li>
+              <li>
+                <span class="date">11/21</span> <a href="#"></a>
+              </li>
+              <li>
+                <span class="date">11/15</span> <a href="#"></a>
+              </li>
+              <li class="last-item">
+                <span class="date">11/10</span> <a href="#"></a>
+              </li>
+            </ul>
+          </div>
+          <p>
+         </p>
+        </div>
+        <br class="clear-all" />
+      </div>
+    </div>
+    <div id="site-credits">
+      &copy; Your Page Name. Design by <a href="http://www.csstemplatesforfree.org">CSS Templates For Free</a>.
+    </div>
+  </body>
+</html>
