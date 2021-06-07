928e47dff11bbd7d0b12531a10774c7f14387034
harilpatel <84775177+harilpatel@users.noreply.github.com>
2021-06-07 10:12:20 -0700

    Add files via upload

---
+++ task1/facilities.php
@@ -0,0 +1,169 @@
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>ALPHA COLLEGE OF ENGINEERING AND TECHNOLOGY</title>
+			
+			<link rel="stylesheet" type="text/css" href="main.css">
+			<style type="text/css">
+			.imgtag{
+			padding: 20px; height: 300px; width: 200px;
+			}
+			.smlb{
+			border-bottom: 1px;
+			border-bottom-color: black; width: 100%;
+			border-bottom-style: solid; padding-left: 25px} 
+			.slab1{
+			border-bottom: 5px; border-bottom-color: red; border-bottom-style: solid; padding: 4px;
+			margin-top: 25px;
+			}
+			.table1{
+			margin-left:120px;
+			max-width:100px;
+			}
+			.table2{
+			width: 100%
+			}
+			.tbldiv1{
+				border-bottom: 1px; border-bottom-style: solid;
+				border-bottom-color: #F2F2F2;
+				margin-left:120px; margin-right:120px;
+				}
+			</style>
+	</head>
+	<body>
+	<header>
+           
+	<center>
+		
+		<br><br><br><br><br><br>
+		<table > <tr><td><img src="images15/collegelogo.jpg" alt=""/></td><td>
+		<h1>ALPHA COLLEGE OF ENGINEERING AND TECHNOLOGY</h1></td></tr></table></center></header><center>
+		<h2>Computer Engineering Department</h3></center>
+		<section><nav class="topnav"><ul class="navto">
+			<li><a  href="indexmain.html"><b>About Department</b></a></li>
+			
+			<li><a href="http://syllabus.gtu.ac.in/">Syllabus</a></li>
+			
+			<li><a href="">Publications</a></li>
+			
+			<li><a href="">Department Academic Calender 2019-2020</a></li></ul>
+			<li><a  href="faculty1.html">Contact Us</a></li></nav></nav>
+			<h1><center>Facilities</center></h1>
+			<hr style=" margin-left:120px; margin-right:120px;color:green;"><br>
+		</section>
+			<div class="navbar1">
+				<div class="slab1 topnav" >
+				<ul>
+					<li class="active"><a href="facilities.html" class="navto">Laboratories</a></li>
+					<li><a href="seminar.html">Seminar Room</a></li>
+					<li><a href="server.html">Server Room</a></li>
+				</ul>
+			</div>
+		<div>
+				<p style="margin-left:120px; margin-right:120px;"> There are total 10 laboratories in the department for performing practical in various domains like Computer Programming, Computer Graphics, Networking, Database Management, Operating Systems, Artificial Intelligence, etc. Department has a separate laboratory for computer hardware subjects.</p>
+		</div>
+		<div class="tbldiv1">
+			<table height="200px" class="table2" >
+				<tr>
+					<td class="table1"><b>8112 Project Lab</b></br></br>
+						<p>This lab is dedicatedly used for project and design engineering. Sufficient number of PC, laptop points and other required infrastructure is available in this lab.</p></br></br>
+					</td>
+					<td class="table1" align="center"><img src="images15/LAB3.jpg" alt=""/>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div class="tbldiv1">
+                    
+			<table height="200px" class="table2">
+				<tr >
+				<td class="table1" align="center"><img src="images15/lab2.jpg" alt=""/></td>
+				<td class="table1"><b>8113 Programming Lab - 1</b></br></br>
+				This lab is dedicatedly used for subjects such as DBMS, OS, CG, CoS, SE, Compiler Design and Python</td>
+				</tr>
+			</table>
+		</div>
+		<div class="tbldiv1">
+				<table height="200px" class="table2">
+					<tr>
+					 <td class="table1"><b>8115 Hardware Lab</b></br></br>
+						This lab is dedicatedly used for hardware related subjects such as digital fundamentals, COA, CN, SoC and IoT.</br></br>
+						<td class="table1" align="center">
+                                                    <img src="images15/lab1.jpg" alt="" height="100px;"/>
+					</td>
+					</tr>
+ 
+
+				</table>
+		</div>
+
+
+                           
+	
+
+	<div class="footer1">
+		<div class="footer-content">
+			<div class="footer-section about">
+				<img style="height: 120px;" src="images15/college1.jpg">
+				<p>The mission of ALPHA Engineering College  is to deliver technical programs and service to meet the changing need of society and industry and thereby contribute to the overall techno-economical development of the State in particular and national in general and improve quality of life of communities.</p>
+			</div>
+	 <div class="footer-section links">
+		<table >
+			<th class="footer-table" align="left">Useful Links</th>
+			<tr ><td class="td2"><a href="https://www.gtu.ac.in/">GTU</a></td></tr>
+			<tr ><td class="td2"><a href="https://www.aicte-india.org">AICTE</a></td></tr>
+			<tr ><td class="td2"><a href="https://nptel.ac.in/">NPTEL</a></td></tr>
+			<tr ><td class="td2"><a href="https://www.jacpcldce.ac.in/">ACPC</a></td></tr>
+			<tr ><td class="td2"><a href="https://www.coursera.org/">COURSERA</a></td></tr>
+		</table>
+	</div>
+	<div class="footer-section downloads">
+	<table>
+	<th class="footer-table" align="left">Downloads</th>
+	<tr>
+	<td class="td2"><a href="#">CIRCULARS</a> </td>
+	<tr>
+
+	</tr>
+	<td class="td3"><a href="#">NOTICES</a></td>
+	<tr>
+	<td class="td3"><a href="#">ADVERTISEMENT</a></td>
+	</tr>
+
+	<tr>
+	<td class="td3"><a href="#">APPLICATION FORM</a></td>
+	</tr>
+
+	<tr>
+	<td class="td3"><a href="#">HOSTEL FORM</a> </td>
+	</tr>
+	<tr>
+	<td class="td3"><a href="#">INFORMATION BROCHURE</a></td>
+	</tr>
+	</table>
+	</div>
+	</div>
+	</div>
+	</body>
+</html>
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
